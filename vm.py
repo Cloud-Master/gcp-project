267909a579cfb2b19b780630569b05fc64cd4a8e
PavanKumar <pavankumar_emmadi@gmail.com>
2018-12-18 12:55:01 +0530

    startup added

--- vm.py
+++ vm.py
@@ -4,14 +4,17 @@ from nw import *
 from variables import *
 ts = Terrascript()
 
+
+
 ts.add(
         google_compute_instance(
             'test-vm', 
             name='test-pavan-tft', 
             boot_disk=[{'initialize_params':[{'image':'debian-cloud/debian-9'}]}], 
-            network_interface=[{'network': nw.self_link, 'subnetwork': snw.self_link, 'access_config':{} }],
+            network_interface=[{'subnetwork': snw.self_link, 'access_config':{} }],
             machine_type='n1-standard-1', 
-            zone=zone
+            zone=zone,
+            metadata_startup_script = '${data.start_up.startup_script.rendered}'
         ))
 
 file = open("./terraform/vm.tf.json", "w")
